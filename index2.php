2abb0e2cad71b63064e3a48083e766897d004366
wmltiago <tiagobhs@outlook.com>
2017-10-25 16:03:34 -0200

    Organização de pastas

--- index2.php
+++ index2.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,210 +10,214 @@
 
     <!-- Bootstrap -->
     <link href="bootstrap3.7/css/bootstrap.min.css" rel="stylesheet">
-      <link href="assets/_css/default.css" rel="stylesheet">
-      <link href="assets/_css/footer.css" rel="stylesheet">
-      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-      <link rel="stylesheet" href="assets/font-awesome-4.7/css/font-awesome.min.css">
+    <link href="assets/_css/default.css" rel="stylesheet">
+    <link href="assets/_css/footer.css" rel="stylesheet">
+    <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="assets/font-awesome-4.7/css/font-awesome.min.css">
 
-     
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->    
-    
-  </head>
-  <body>
+    <![endif]-->
 
-      <div class="container">
-      
-      <nav id="navtop">
-        <div class="container-fluid">
-      <!--header navbar top left -->
-              <ul class="nav navbar-nav" id="nav-left">
-                <li><a href="#">
-                  <i class="fa fa-user" aria-hidden="true"></i>ACESSO</a>
-                </li>
-                <li><a href="#">
+</head>
+
+<body>
+
+    <div class="container">
+
+        <nav id="navtop">
+            <div class="container-fluid">
+                <!--header navbar top left -->
+                <ul class="nav navbar-nav" id="nav-left">
+                    <li><a href="http://www.ifpe.edu.br/acessibilidade" target="blank">
+                  <i class="fa fa-user" aria-hidden="true"></i>ACESSIBILIDADE</a>
+                    </li>
+                    <li><a href="#">
                   <i class="fa fa-check-square-o" aria-hidden="true"></i>CHECK OUT</a>
-                </li>
-                <li><a href="#">
+                    </li>
+                    <li><a href="#">
                   <i class="fa fa-heart-o" aria-hidden="true"></i>MY WISHLIST</a>
-                </li>
-              </ul>
-      <!--header navbar top right -->
-              <ul class="nav navbar-nav nav-pills" id="nav-right">
-                <li>
-                  <a href="#">
+                    </li>
+                </ul>
+                <!--header navbar top right -->
+                <ul class="nav navbar-nav nav-pills" id="nav-right">
+                    <li>
+                        <a href="#">
                         <i class="fa fa-search" aria-hidden="true"></i>SEARCH</a>
-                </li>
-                <li>
-                  <a href="#">
+                    </li>
+                    <li>
+                        <a href="#">
                         <i class="fa fa-shopping-bag" aria-hidden="true"></i>MYBAG</a>
-                </li>
+                    </li>
 
-                <li><button type="btn btn-default navbar-btn"><i class="fa fa-sign-in" aria-hidden="true"></i>
+                    <li><button type="btn btn-default navbar-btn"><i class="fa fa-sign-in" aria-hidden="true"></i>
                     ENTRAR</button></li>
 
-                <li><button type="btn btn-default navbar-btn">INSCRIÇÃO</button></li>
-              </ul>
+                    <li><button type="btn btn-default navbar-btn">INSCRIÇÃO</button></li>
+                </ul>
 
-          </div>
+            </div>
         </nav>
-      </div>
-      <!--header navbar LOGO -->
-        
-      <div class="alive">
+    </div>
+    <!--header navbar LOGO -->
+
+    <div class="alive">
         <div class="text-center" id="logo">
-              <a href="#" ><img src="ifpe.png" alt="logo" height="150px" width="250px"/></a><br>
-               IFPE-Pós<br>
-                Campus - Jaboatão dos Guararapes 
+            <a href="#"><img src="imagens/ifpe.png" alt="logo" height="150px" width="250px"/></a><br> IFPE-Pós 2018
+            <br> Campus - Jaboatão dos Guararapes
         </div>
-          </div>
-           
-      <!-- TOOGLE MAIN NAVIGATION -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navigation" aria-expanded="false">
+    </div>
+
+    <!-- TOOGLE MAIN NAVIGATION -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navigation" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-        </div>
-
-      <!--  MAIN NAVIGATION -->
-        <div class="collapse navbar-collapse" id="main-navigation">
-          <div class="container">
-
-          <ul class="nav nav-justified nav-pills">
-              <li><a href="#"><span class="glyphicon glyphicon-home"></span> HOME</a></li>
-              <li><a href="#"><span class="glyphicon glyphicon-file"></span> EDITAIS</a></li>
-              <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> CONTATO</a></li>
-              <li><a href="#"><span class="glyphicon glyphicon-bullhorn"></span> OUVIDORIA</a></li>
-              <li><a href="#"><span class="glyphicon glyphicon-map-marker"></span> LOCALIZAÇÃO</a></li>
-              
-          </ul>
-          </div>
-        </div>
-<!--header navbar END -->
-
-      <div class="container-absoluto">
-        
-<!--container info-->          
-<div class="container container-info">
-      <div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="pos-graduacao.png" alt="...">
-      <div class="caption">
-        <h3>Propesq divulga edital para concessão de bolsas a docentes</h3>
-        <p>Foco são docentes com dedicação exclusiva ou jornada de 40 horas, vinculados ao doutorado.</p>
-        <p><a href="#" class="btn btn-default" role="button">Mais...</a></p>
-      </div>
     </div>
-  </div>
 
-    
-    
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="vestibular.png" alt="...">
-      <div class="caption">
-        <h3>IFPE divulga edital com 4.076 vagas para o Vestibular 2018.1 </h3>
-        <p>Inscrições seguem até 05 de novembro e podem ser feitas de forma online</p>
-        <p><a href="#" class="btn btn-default" role="button">Mais...</a></p>
-      </div>
-    </div>
-  </div>
+    <!--  MAIN NAVIGATION -->
+    <div class="collapse navbar-collapse" id="main-navigation">
+        <div class="container">
 
-    
-   
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-      <img src="prof.png" alt="...">
-      <div class="caption">
-        <h3>Professor do Campus Recife é finalista do Jabuti</h3>
-        <p>Professor José Eduardo de Oliveira concorre na categoria Engenharias, Tecnologias e Informática</p>
-        <p> <a href="#" class="btn btn-default" role="button">Mais...</a></p>
-      </div>
-    </div>
-  </div>
+            <ul class="nav nav-justified nav-pills">
+                <li><a href="#"><span class="glyphicon glyphicon-home"></span> HOME</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-file"></span> EDITAIS</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-envelope"></span> CONTATO</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-bullhorn"></span> OUVIDORIA</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-map-marker"></span> LOCALIZAÇÃO</a></li>
 
-      </div>     
+            </ul>
         </div>
-<!--END container info-->
-          </div>
-      
-  <!-- FOOTER -->    
+    </div>
+    <!--header navbar END -->
+
+    <div class="container-absoluto">
+
+        <!--container info-->
+        <div class="container container-info">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="imagens/pos-graduacao.png" alt="...">
+                        <div class="caption">
+                            <h3>Propesq divulga edital para concessão de bolsas a docentes</h3>
+                            <p>Foco são docentes com dedicação exclusiva ou jornada de 40 horas, vinculados ao doutorado.</p>
+                            <p><a href="#" class="btn btn-default" role="button">Mais...</a></p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="imagens/vestibular.png" alt="...">
+                        <div class="caption">
+                            <h3>IFPE divulga edital com 4.076 vagas para o Vestibular 2018.1 </h3>
+                            <p>Inscrições seguem até 05 de novembro e podem ser feitas de forma online</p>
+                            <p><a href="#" class="btn btn-default" role="button">Mais...</a></p>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="imagens/prof.png" alt="...">
+                        <div class="caption">
+                            <h3>Professor do Campus Recife é finalista do Jabuti</h3>
+                            <p>Professor José Eduardo de Oliveira concorre na categoria Engenharias, Tecnologias e Informática</p>
+                            <p> <a href="#" class="btn btn-default" role="button">Mais...</a></p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <!--END container info-->
+    </div>
+
+    <!-- FOOTER -->
     <div class="">
-    <section style="height:80px;"></section>
-	
-    <!----------- Footer ------------>
-    <footer class="footer-bs">
-        <div class="row">
-        	<div class="col-md-3 footer-brand animated fadeInLeft">
-            	<div class="text-center" id="logo-footer">
-              <a href="http://www.brasil.gov.br/" ><img src="ico_Governo.png" alt="logo" height="100px" width="200px"/></a><br>
-               
-        </div>
-            </div>
-        	<div class="col-md-4 footer-nav animated fadeInUp">
-            	<h3>IFPE</h3>
-            	<div class="col-md-6">
-                    <ul class="list"> <!-- era pages no lugar de list -->
-                        <li><a href="#">Reitoria</a></li>
-                        <li><a href="#">Ensino</a></li>
-                        <li><a href="#">Pesquisa e inovação</a></li>
-                        <li><a href="#">Extenção</a></li>
-                        <li><a href="#">Administração</a></li>
+        <section style="height:80px;"></section>
+
+        <!----------- Footer ------------>
+        <footer class="footer-bs">
+            <div class="row">
+                <div class="col-md-3 footer-brand animated fadeInLeft">
+                    <div class="text-center" id="logo-footer">
+                        <a href="http://www.brasil.gov.br/"><img src="imagens/ico_Governo.png" alt="logo" height="100px" width="200px"/></a><br>
+
+                    </div>
+                </div>
+                <div class="col-md-4 footer-nav animated fadeInUp">
+                    <h3>IFPE</h3>
+                    <div class="col-md-6">
+                        <ul class="list">
+                            <!-- era pages no lugar de list -->
+                            <li><a href="#">Reitoria</a></li>
+                            <li><a href="#">Ensino</a></li>
+                            <li><a href="#">Pesquisa e inovação</a></li>
+                            <li><a href="#">Extenção</a></li>
+                            <li><a href="#">Administração</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <ul class="list">
+                            <li><a href="#">Desenvolvimento Institucional</a></li>
+                            <li><a href="#">Relações Internacionais</a></li>
+                            <li><a href="#">Tecnologia da Informação</a></li>
+                            <li><a href="#">Assistência Estudantil</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-2 footer-social animated fadeInDown">
+                    <h3>Redes Sociais</h3>
+                    <ul>
+                        <li class="write"><a href="https://www.facebook.com/FacebookBrasil/?brand_redir=20531316728">Facebook</a></li>
+                        <li><a href="https://twitter.com/twitter">Twitter</a></li>
+                        <li><a href="#">Instagram</a></li>
+                        <li><a href="#">RSS</a></li>
                     </ul>
                 </div>
-            	<div class="col-md-6">
-                    <ul class="list">
-                        <li><a href="#">Desenvolvimento Institucional</a></li>
-                        <li><a href="#">Relações Internacionais</a></li>
-                        <li><a href="#">Tecnologia da Informação</a></li>
-                        <li><a href="#">Assistência Estudantil</a></li>
+                <div class="col-md-2 footer-social animated fadeInDown">
+                    <h3>Navegação</h3>
+                    <ul>
+                        <li><a href="http://www.ifpe.edu.br/acessibilidade">Acessibilidade</a></li>
+                        <li><a href="http://www.ifpe.edu.br/mapadosite">Mapa do site</a></li>
                     </ul>
                 </div>
             </div>
-        	<div class="col-md-2 footer-social animated fadeInDown">
-            	<h3>Redes Sociais</h3>
-            	<ul>
-                	<li class="write"><a href="https://www.facebook.com/FacebookBrasil/?brand_redir=20531316728">Facebook</a></li>
-                	<li><a href="https://twitter.com/twitter">Twitter</a></li>
-                	<li><a href="#">Instagram</a></li>
-                	<li><a href="#">RSS</a></li>
-                </ul>
-            </div>
-        	<div class="col-md-2 footer-social animated fadeInDown">
-            	<h3>Navegação</h3>
-            	<ul>                	
-                	<li><a href="http://www.ifpe.edu.br/acessibilidade">Acessibilidade</a></li>
-                	<li><a href="http://www.ifpe.edu.br/mapadosite">Mapa do site</a></li>
-                </ul>
-            </div>
-        </div>
-    </footer>
-    <section style="text-align:center; margin:10px auto;"><p>Designed by <a href="https://scontent.frec3-2.fna.fbcdn.net/v/t1.0-9/16806893_1274123329361262_1978278696836318808_n.jpg?oh=df2801b7186b8333580456a429841f8a&oe=5A68A023">Tiago Borges </a></p></section>
+        </footer>
+        <section style="text-align:center; margin:10px auto;">
+            <p>Designed by <a href="https://scontent.frec3-2.fna.fbcdn.net/v/t1.0-9/16806893_1274123329361262_1978278696836318808_n.jpg?oh=df2801b7186b8333580456a429841f8a&oe=5A68A023" target="_blank">Tiago Borges </a></p>
+        </section>
 
-</div>     
-      
-      
-      
-      
-      
+    </div>
 
-<script src="https://use.fontawesome.com/f12e4a6b3c.js"></script>
-      
+
+
+
+
+
+    <script src="https://use.fontawesome.com/f12e4a6b3c.js"></script>
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <script src="assets/js.js"></script>
+    <script src="assets/js.js"></script>
     <script src="bootstrap3.7/js/bootstrap.min.js"></script>
-      <script src="assets/_js/js.js"></script>
-      
-  </body>
+    <script src="assets/_js/js.js"></script>
+
+</body>
+
 </html>
